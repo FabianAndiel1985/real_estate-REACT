11bc307542c9a9bc5faf0032f7ee671b664f122f
Fabian Andiel <fabian.andiel@yahoo.com>
2020-09-08 11:21:40 +0200

    Set up landing page and draft for dynamic styles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      daytime:"morning",
+    }
+  }
+
+  getTime() {
+      let d = new Date();
+      return d.getHours();
+  }
+
+
+  getGreeting() {
+    let daytime="";
+
+    if(18 >= this.getTime() && this.getTime() >= 12) {
+       daytime="afternoon";
+    }
+
+    else if(23 >= this.getTime() && this.getTime() >= 19) {
+       daytime="evening";
+    }
+
+    this.setState({
+        daytime:daytime
+      })
+  }
+
+
+  componentDidMount() {
+    this.getGreeting();
+  }
+
+
+
+  render() {
+    
+    let morningStyle = {
+      color: "green"
+    }
+
+
+//     getBackgroundColor(status) {
+//     if (status === 'approved') {
+//         return 'blue';
+//     }
+//     if (status === 'pending') {
+//         return 'red';
+//     }
+//     return 'black';
+// }
+
+
+
+// render() {
+//     // ...
+
+//     return (
+//         <div style={{ 'backgroundColor': this.getBackgroundColor(status) }}></div>
+//     );
+// }
+
+    return (
+      <h1 style={}  className="text-center"> Good {this.state.daytime} </h1>
+    );
+  }
 }
 
 export default App;
