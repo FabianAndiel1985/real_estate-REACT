18de0e0aff3661d1b1926de54f0f4a8b9521650a
Fabian Andiel <fabian.andiel@yahoo.com>
2020-09-10 09:29:38 +0200

    Revert "futher changes page-not-found"
    This reverts commit 93c5d4dd94cefc7227070739c88ed86fd55c1aa7.

--- src/components/page-not-found/page-not-found.js
+++ src/components/page-not-found/page-not-found.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 
 function PageNotFound(props) {
+	console.log(props.history);
   return (<div><h1>Page not found</h1>
   		<button onClick={props.history.goBack}> go back </button></div>)
 }
