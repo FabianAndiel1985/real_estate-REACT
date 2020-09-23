196173fe527beb91db77e32d161b46dc86dfdc3a
Fabian Andiel <fabian.andiel@yahoo.com>
2020-09-23 09:48:39 +0200

    Styles product page

--- src/components/products/products.js
+++ src/components/products/products.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { connect, useDispatch } from 'react-redux';
 import {addProduct} from '../../actions/actions';
 import {Table} from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
+import styles from './products.module.css';
 
 class Products extends React.Component {
 	
@@ -18,7 +20,7 @@ class Products extends React.Component {
               {id:"5",name:"product5",price:1.39},
               {id:"6",name:"product6",price:4.99},
               {id:"7",name:"product7",price:2.99},
-              {id:"8",name:"producz8",price:4.99},
+              {id:"8",name:"product8",price:4.99},
 
             ],
         }
@@ -29,36 +31,34 @@ class Products extends React.Component {
   render() {
     
     return (
-    	<div>
-    
-    	<p> {this.props.value}</p>
-      <Table>
-        <thead>
-          <tr>
-            <th>number</th>
-            <th>product</th>
-            <th>price </th>
-            <th>actions</th>
-          </tr>
-        </thead>
-        <tbody>
-        {this.state.products ? this.state.products.map(
-               (item,index)=>{
-                 return (
-                 <tr key={item.id}>
-                  <td>{item.id}</td>
-                  <td>{item.name}</td>
-                  <td>{item.price}</td>
-                  <td> <button onClick={(event)=>this.props.onClick(event)}> add to cart</button> </td>
-                </tr>
-                )           
-                }  
-        )
-             :null}
-          
-        </tbody>
-      </Table>
-    	</div>);
+    	<Container className="my-5">
+        <Table>
+          <thead>
+            <tr>
+              <th className={styles.tableHeading}>number</th>
+              <th className={styles.tableHeading}>product</th>
+              <th className={styles.tableHeading}>price </th>
+              <th className={styles.tableHeading}>actions</th>
+            </tr>
+          </thead>
+          <tbody>
+          {this.state.products ? this.state.products.map(
+                 (item,index)=>{
+                   return (
+                   <tr key={item.id}>
+                    <td className={styles.tableText}>{item.id}</td>
+                    <td className={styles.tableText}>{item.name}</td>
+                    <td className={styles.tableText}>{item.price}</td>
+                    <td> <button className={styles.tableText} onClick={(event)=>this.props.onClick(event)}> add to cart</button> </td>
+                  </tr>
+                  )           
+                  }  
+          )
+               :null}
+            
+          </tbody>
+        </Table>
+    	</Container>);
   }
 }
 
