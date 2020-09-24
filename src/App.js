29928198574ca542ad001c526d20c71386c13b1d
Fabian Andiel <fabian.andiel@yahoo.com>
2020-09-24 14:36:24 +0200

    Revert "Styles app component"
    This reverts commit a1b78086facdff7a700762fc4a985e2b9cd2067e.

--- src/App.js
+++ src/App.js
@@ -42,14 +42,14 @@ class App extends Component {
   render() {
    
     return ( 
-      <Container className={styles.appContainer}  className="mt-4 mb-5">
+      <Container className="mt-4">
 
         <h1 style={{'color': this.getFontColour()}}  className="text-center"> Good {this.state.daytime} </h1>
         <h1>Welcome to Fabian Andiel Real</h1>
         
         <img className="mt-5" src={require("./assets/images/welcomePic.jpg")}/>
         
-        <p className="mt-3">
+        <p className="mt-3 mb-5">
           <a onClick={() => this.setLgShow()} className={styles.adminLogin}>
             Login as an Administrator
            </a>
